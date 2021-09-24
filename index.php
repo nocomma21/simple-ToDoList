2f9d1b4a62694e683814017afeb90f13054f7640
cloverdyling <81244527+dianalinggasaputri@users.noreply.github.com>
2021-09-24 21:08:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_POST['go_doing'])) {
     header('location: index.php');
 }
 
-// go_done onclieck
+// go_done onclick
 if (isset($_POST['go_done'])) {
     $task_ke = $_POST['go_done'];
     $id = $_POST['go_done'];
@@ -47,16 +47,7 @@ if (isset($_POST['go_done'])) {
     header('location: index.php');
 }
 
-// done_task
-// if(isset($_GET['done_task'])) {
-//     $task_ke = $_GET['done_task'];
-//     $id = $_GET['done_task'];
-//     $queryDoneTask = ""
-// }
-
-
-
-// delete task
+// delete_task
 if (isset($_GET['del_task'])) {
     $id = $_GET['del_task'];
     mysqli_query($conn, "DELETE FROM tasks WHERE id=$id");
@@ -98,7 +89,7 @@ $task3 = mysqli_query($conn, "SELECT * FROM tasks WHERE task_ke = 3");
                         <input type="text" class="form-control" name="task" placeholder="Type To Do" required>
                         <input type="hidden" name="task_ke" value="1">
                         <span class="input-group-text" id="basic-addon2">
-                            <button class="btn btnAddTo" name="addTask1" type="submit"><i class='fa fa-plus'></i> Add To Do </button>
+                            <button class="btn btnAddTo" name="addTask1" type="submit"><i class='fa fa-plus'></i> To Do </button>
                         </span>
                     </div>
                 </form>
@@ -148,7 +139,7 @@ $task3 = mysqli_query($conn, "SELECT * FROM tasks WHERE task_ke = 3");
                         <input type="text" class="form-control" name="task" placeholder="Type Doing" required>
                         <input type="hidden" name="task_ke" value="2">
                         <span class="input-group-text" id="basic-addon2">
-                            <button class="btn btnAddTo" name="addTask2" type="submit"><i class='fa fa-plus'></i> Add Doing </button>
+                            <button class="btn btnAddTo" name="addTask2" type="submit"><i class='fa fa-plus'></i> Doing </button>
                         </span>
                     </div>
                 </form>
@@ -198,7 +189,7 @@ $task3 = mysqli_query($conn, "SELECT * FROM tasks WHERE task_ke = 3");
                         <input type="text" class="form-control" name="task" placeholder="Type Done" required>
                         <input type="hidden" name="task_ke" value="3">
                         <span class="input-group-text" id="basic-addon2">
-                            <button class="btn btnAddTo" name="addTask3" type="submit"><i class='fa fa-plus'></i> Add Done </button>
+                            <button class="btn btnAddTo" name="addTask3" type="submit"><i class='fa fa-plus'></i> Done </button>
                         </span>
                     </div>
                 </form>
